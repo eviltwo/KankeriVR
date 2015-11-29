e02b3938b63bf57709353f7d04e26b4771423b41
e135702 <e135702@ie.u-ryukyu.ac.jp>
2015-11-29 16:42:09 +0900

    playerscript編集

--- Assets/Minato/Script/PlayerScript.cs
+++ Assets/Minato/Script/PlayerScript.cs
@@ -10,7 +10,9 @@ public class PlayerScript : MonoBehaviour
 	bool onGround = false;
 	bool touchBoss = false;
 	public Camera myCamera;
+	
 	float Energy = 5.0f;
+	bool dashAble = false;
 
 	// Use this for initialization
 	void Start ()
@@ -21,26 +23,28 @@ public class PlayerScript : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		if (Energy < 5.0f) {
+		Energy += Time.deltaTime;
+		if(Energy >= 5){
+			dashAble = true;
+			Energy -= Time.deltaTime;
+		}
+		if(Energy <= 0){
+			dashAble = false;
 			Energy += Time.deltaTime;
 		}
-		
 		var cameraForward = Vector3.Scale (myCamera.transform.forward,
 		                                    new Vector3 (1, 0, 1)).normalized;
 		Vector3 direction = Walk (cameraForward);
-
-		if (Input.GetAxis ("Fire1") == 1) {
-			if (Energy > 1) {
-				direction = Dash (cameraForward);
-				Energy -= Time.deltaTime; 
-			} else {
-				if (Energy < 5) {
-					direction = Walk (cameraForward);
-					Energy += Time.deltaTime;
+		
+		if(dashAble == true){
+			if(Input.GetAxis("Fire1") == 1){
+				Dash(cameraForward);
+				Energy -= Time.deltaTime*2;
+				print (Energy);
+				if(Energy <= 0){
+					dashAble = false;
 				}
 			}
-		} else {
-			direction = Walk (cameraForward);
 		}
 
 		direction.y = rb.velocity.y;
@@ -48,7 +52,7 @@ public class PlayerScript : MonoBehaviour
 
 		if (onGround) {
 			if (Input.GetKeyDown ("space")) {
-				direction.y = 320f;
+				direction.y = 350f;
 				rb.AddForce (direction, ForceMode.Force);
 				onGround = false;
 			}
